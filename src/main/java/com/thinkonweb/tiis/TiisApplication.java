f858ae92a7c7de62635e56774e6441e24b82a64c
animato@mac <neoanimato@gmail.com>
2015-10-07 21:28:44 +0900

    Spring boot 및 초기 라이브러리 구성
    gitignore 추가 (jms프로젝트 참고)
    
    #2

---
+++ src/main/java/com/thinkonweb/tiis/TiisApplication.java
@@ -0,0 +1,12 @@
+package com.thinkonweb.tiis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiisApplication.class, args);
+    }
+}
---
+++ src/test/java/com/thinkonweb/tiis/TiisApplicationTests.java
@@ -0,0 +1,18 @@
+package com.thinkonweb.tiis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TiisApplication.class)
+@WebAppConfiguration
+public class TiisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
