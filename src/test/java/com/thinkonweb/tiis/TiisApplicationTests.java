dc6362a115e5b9a8e3215f598b0b51b4b5f89d8b
ds.yang@thinkonweb <ds.yang@thinkonweb.com>
2015-11-16 14:23:03 +0900

    TiisApplicationTests.java 코드 추가

--- src/test/java/com/thinkonweb/tiis/TiisApplicationTests.java
+++ src/test/java/com/thinkonweb/tiis/TiisApplicationTests.java
@@ -2,17 +2,28 @@ package com.thinkonweb.tiis;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = TiisApplication.class)
+@SpringApplicationConfiguration(classes = {TiisApplication.class, MybatisConfiguration.class})
+@ActiveProfiles("dev")
 @WebAppConfiguration
 public class TiisApplicationTests {
 
+	@Value("${spring.datasource.url}")
+	private String url;
+
+	@Value("${spring.datasource.username}")
+	private String username;
+
 	@Test
 	public void contextLoads() {
+		System.out.println(url);
+		System.out.println(username);
 	}
 
 }
